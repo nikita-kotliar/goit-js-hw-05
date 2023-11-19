9e4aecc462a7e923c83a65c844dc3fa845ffcc4f
nikita-kotliar <knr200710@gmail.com>
2023-11-19 13:02:05 +0200

    ready

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,5 @@
 const getTotalBalanceByGender = (users, gender) => {
-    users
+    return users
     .filter(user => user.gender === gender)
     .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 }
